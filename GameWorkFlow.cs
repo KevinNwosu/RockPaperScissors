0807022a161040c3a2ff01b3efc5a84ba4064a43
Kevin-Kyle Nwosu <KKNwosu@dev-10.com>
2022-03-17 15:23:49 -0500

    Create best 2 out of 3 version

--- GameWorkFlow.cs
+++ GameWorkFlow.cs
@@ -3,71 +3,48 @@
     public class GameWorkFlow
     {
         public void Run()
-        { 
-            string promptPlayer1 = "Enter implementation for player 1. ";
-            int choice = ConsoleIO.PromptPlayerType(promptPlayer1);
-            Console.Clear();
-            int player1Choice = 0;
-
-            switch (choice)
+        {
+            WinTracker w = new WinTracker();
+            bool playing = true;
+            while (playing)
             {
-                case 1:
-                    RandomPlayer r1 = new RandomPlayer();
-                    player1Choice = r1.GetPlayerBehaviour();
-                    break;
-                case 2:
-                    ConsolePlayer c1 = new ConsolePlayer();
-                    player1Choice = c1.GetPlayerBehaviour();
-                    break;
-                case 3:
-                    AlwaysRock ar1 = new AlwaysRock();
-                    player1Choice = ar1.GetPlayerBehaviour();
-                    break;
-                case 4:
-                    AlwaysPaper ap1 = new AlwaysPaper();
-                    player1Choice = ap1.GetPlayerBehaviour();
-                    break;
-                case 5:
-                    AlwaysScissors s1 = new AlwaysScissors();
-                    player1Choice = s1.GetPlayerBehaviour();
-                    break;
+                string promptPlayer1 = "Enter implementation for player 1. ";
+                int choice = ConsoleIO.PromptPlayerType(promptPlayer1);
+                Console.Clear();
+                int player1Choice = GetPlayChoice(choice);
+
+                string promptPlayer2 = "Enter implementation for player 2. ";
+                int choice2 = ConsoleIO.PromptPlayerType(promptPlayer2);
+                Console.Clear();
+                int player2Choice = GetPlayChoice(choice2);
+
+                int result = GetResult(player1Choice, player2Choice);
+
+                if (result == 1)
+                {
+                    w.TieCount = w.TieCount + 1;
+                }
+                else if (result == 2)
+                {
+                    w.Player1Count = w.Player1Count + 1;
+                }
+                else if (result == 3)
+                {
+                    w.Player2Count = w.Player2Count + 1;
+                }
+
+                if (w.Player1Count == 2 || w.Player2Count == 2)
+                {
+                    playing = false;
+                }
+                Console.WriteLine($"Player 1 chose {(PlayerChoice)player1Choice} and Player 2 chose {(PlayerChoice)player2Choice}");
+
+                ConsoleIO.PrintResult(result);
+
+                Console.ReadKey();
+                Console.Clear();
             }
-
-            string promptPlayer2 = "Enter implementation for player 2. ";
-            int choice2 = ConsoleIO.PromptPlayerType(promptPlayer2);
-            Console.Clear();
-            int player2Choice = 0;
-
-            switch (choice2)
-            {
-                case 1:
-                    RandomPlayer r2 = new RandomPlayer();
-                    player2Choice = r2.GetPlayerBehaviour();
-                    break;
-                case 2:
-                    ConsolePlayer c2 = new ConsolePlayer();
-                    player2Choice = c2.GetPlayerBehaviour();
-                    break;
-                case 3:
-                    AlwaysRock ar2 = new AlwaysRock();
-                    player2Choice = ar2.GetPlayerBehaviour();
-                    break;
-                case 4:
-                    AlwaysPaper ap2 = new AlwaysPaper();
-                    player2Choice = ap2.GetPlayerBehaviour();
-                    break;
-                case 5:
-                    AlwaysScissors s2 = new AlwaysScissors();
-                    player2Choice = s2.GetPlayerBehaviour();
-                    break;
-            }
-
-            int result = GetResult(player1Choice, player2Choice);
-
-            Console.WriteLine($"Player 1 chose {(PlayerChoice)player1Choice} and Player 2 chose {(PlayerChoice)player2Choice}"); 
-
-            
-            ConsoleIO.PrintResult(result);
+            Console.WriteLine($"Player 1 won {w.Player1Count} times and Player 2 won {w.Player2Count} times, there were {w.TieCount} ties");
             
 
         }
@@ -87,6 +64,29 @@
             }
 
             return 3;
+        }
+        public int GetPlayChoice(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    RandomPlayer r1 = new RandomPlayer();
+                    return r1.GetPlayerBehaviour();
+                case 2:
+                    ConsolePlayer c1 = new ConsolePlayer();
+                    return c1.GetPlayerBehaviour();
+                case 3:
+                    AlwaysRock ar1 = new AlwaysRock();
+                    return ar1.GetPlayerBehaviour();
+                case 4:
+                    AlwaysPaper ap1 = new AlwaysPaper();
+                    return ap1.GetPlayerBehaviour();
+                case 5:
+                    AlwaysScissors s1 = new AlwaysScissors();
+                    return s1.GetPlayerBehaviour();
+                default:
+                    return 6;
+            }
         }
         public enum PlayerChoice
         {
---
+++ WinTracker.cs
@@ -0,0 +1,9 @@
+﻿namespace RockPaperScissors
+{
+    public class WinTracker
+    {
+        public int Player1Count { get; set; }
+        public int Player2Count { get; set; }
+        public int TieCount { get; set; }
+    }
+}
